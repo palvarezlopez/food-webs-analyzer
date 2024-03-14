b9a9da34e95574a7dc4eefc8c691be2b2d6c9aef
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-03-14 16:50:35 +0900

    now runMypyTest.bat works again. Refs #4

---
+++ src/runMypyTest.bat
@@ -0,0 +1,5 @@
+:mypyCommand
+python -m mypy foodwebDynamics.py --disallow-untyped-defs --extra-checks
+pause
+cls
+goto mypyCommand
--- tests/runMypyTest.bat
+++
@@ -1,8 +0,0 @@
-REM specify <repository>/python/src as a python source
-SET PYTHONPATH=..\src\
-
-:mypyCommand
-python -m mypy ..\src\foodwebDynamics.py --disallow-untyped-defs --extra-checks
-pause
-cls
-goto mypyCommand
