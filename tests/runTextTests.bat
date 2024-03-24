70f20fe22c066f57540d68800e0f007fe75ad2b2
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-03-24 13:59:11 +0900

    updated runTextTests.bat. Refs #4

--- tests/runTextTests.bat
+++ tests/runTextTests.bat
@@ -1,9 +1,7 @@
 @echo off
-REM add texttest gui folder in path
-set PATH=%PATH%;..\..\textTest
 REM set TextTest executable
 SET TEXTTESTPY=texttest.exe
-REM specify <repository>/python/src as a python source
+REM specify <repository>/src as a python source
 SET PYTHONPATH=..\src\
 REM set python executable
 SET PYTHON=python
