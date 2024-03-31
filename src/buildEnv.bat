d09be2d67777b3189d4bc969e2334c8ab0946bfd
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-03-31 11:29:38 +0900

    renamed project. Refs #1

--- src/buildEnv.bat
+++ src/buildEnv.bat
@@ -1,3 +1,3 @@
-python -m venv foodWebsAnalyzer\.venv
+python -m venv food-webs-analyzer\.venv
 REM src\env\Scripts\python.exe -m pip install --upgrade pip &
 REM src\env\Scripts\pip.exe install -r requirements.txt
