6079759773e801d0d0304ce9897ae38829b70459
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-03-31 18:14:52 +0900

    Fixed error in runTextTests.bat. Refs #4

--- tests/runTextTests.bat
+++ tests/runTextTests.bat
@@ -2,7 +2,7 @@
 REM set TextTest executable
 SET TEXTTESTPY=texttest.exe
 REM specify <repository>\src\foodWebsAnalyzer as a python source
-SET PYTHONPATH=..\src
+SET PYTHONPATH=%~dp0\..\src
 REM set python executable
 SET PYTHON=python
 REM init textest
