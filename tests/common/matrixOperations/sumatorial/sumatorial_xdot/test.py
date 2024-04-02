f171433e2b2719466afbb7db97527d1289cd98e1
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-04-02 11:24:49 +0900

    Updated tests. Removed matrixOperation tests. Refs #4 and #9

--- tests/common/matrixOperations/sumatorial/sumatorial_xdot/test.py
+++
@@ -1,8 +0,0 @@
-import sympy as sp
-from common import matrixOperations as mo
-
-matrixA = sp.Matrix([[1, 2], [3, 4]])
-print (mo.sumatorial_xdot(matrixA))
-
-matrixB = sp.Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print (mo.sumatorial_xdot(matrixB))
