bf41dfa0cead968df978fab06a077d3cb3a3d972
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-04-02 09:41:26 +0900

    updated runTextTests.bat. Refs #4

--- tests/runTextTests.bat
+++ tests/runTextTests.bat
@@ -1,10 +1,10 @@
 @echo off
 REM set TextTest executable
-SET TEXTTESTPY=texttest.exe
+SET TEXTTESTPY=%~dp0..\utils\textTest\texttest.exe
 REM specify <repository>\src\foodWebsAnalyzer as a python source
 SET PYTHONPATH=%~dp0..\src
 REM set python executable
-SET PYTHON=python
+SET PYTHON=%~dp0..\src\.venv\Scripts\python.exe
 REM init textest
 start %TEXTTESTPY%
 
